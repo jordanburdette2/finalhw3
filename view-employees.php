1ee576544a1840b1d733251010f52dc462839fcd
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-03 17:38:54 -0500

    Update view-employees.php

--- view-employees.php
+++ view-employees.php
@@ -17,6 +17,7 @@
         <td><?php echo $employee['employee_id']; ?></td>
         <td><?php echo $employee['employee_name']; ?></td>
         <td><?php echo $employee['office_number']; ?></td>
+        <td><a href="jobs-of-employees.php?id=<?php echo $employee['employee_id']; ?>">Jobs</a></td>
       </tr>
     <?php
   }
