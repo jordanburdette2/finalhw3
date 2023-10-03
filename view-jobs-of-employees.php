d10dba17059f48547b855d4a196051301a46b382
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-03 18:02:46 -0500

    Create view-jobs-of-employees.php

---
+++ view-jobs-of-employees.php
@@ -0,0 +1,27 @@
+<h1>Jobs Of Employees</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Number</th>
+      <th>Job Title</th>
+        <th>Job Start Date</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($course = $courses->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $course['job_id']; ?></td>
+    <td><?php echo $course['job_number']; ?></td>
+    <td><?php echo $course['job_title']; ?></td>
+    <td><?php echo $course['start_date']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
