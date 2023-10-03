6924c8a52d8c94b69541b8eb4ae178cf8416c98b
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-03 18:55:03 -0500

    Update model-employees-with-jobs.php

--- model-employees-with-jobs.php
+++ model-employees-with-jobs.php
@@ -12,4 +12,20 @@ function selectEmployees() {
         throw $e;
     }
 }
+
+function selectJobsOfEmployees($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT j.job_id, job_number, job_title, start_date, office_number, building FROM `job` j join division d on j.job_id=d.job_id where d.employee_id=?");
+        $stmt->bind_param("i", $iid);  
+      $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
