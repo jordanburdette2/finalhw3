53a68e98ef269e1c1aa7510e5259e0a7706a14f3
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-03 18:52:37 -0500

    Create model-employees-with-jobs.php

---
+++ model-employees-with-jobs.php
@@ -0,0 +1,15 @@
+<?php
+function selectEmployees() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT employee_id, employee_name, office_number FROM `employee`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
