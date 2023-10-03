84ab3c273a56140f8a38c75853749454e77ec014
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-03 17:40:42 -0500

    Create jobs-of-employees.php

---
+++ jobs-of-employees.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-jobs-of-employees.php");
+
+$pageTitle = "Jobs of Employees";
+include "view-header.php";
+$jobs = selectJobsOfEmployees($_GET['id']);
+include "view-jobs-by-instructor.php";
+include "view-footer.php";
+?>
