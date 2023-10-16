7c5c1bfdd30ae0621ef71fa7c2e68c6f9d43e924
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-16 14:24:02 -0500

    Update model-divisions-of-jobs.php

--- model-divisions-of-jobs.php
+++ model-divisions-of-jobs.php
@@ -1,9 +1,9 @@
 <?php
-function selectDivisionsForJobs($did) {
+function selectDivisionsForJobs($jid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT division_id, employee_id, j.job_id, building, manager from division d join job j on d.job_id=j.job_id WHERE j.job_id=?;");
-        $stmt->bind_param("i", $did);  
+        $stmt->bind_param("i", $jid);  
       $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
