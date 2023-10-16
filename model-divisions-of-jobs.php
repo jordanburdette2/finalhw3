9d0947de9b274563f225e3b4b439521b1d1dd39a
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-16 13:53:33 -0500

    Update model-divisions-of-jobs.php

--- model-divisions-of-jobs.php
+++ model-divisions-of-jobs.php
@@ -1,5 +1,5 @@
 <?php
-function selectJobsOfEmployees($did) {
+function selectDivisionsForJobs($did) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT j.job_id, job_number, job_title, start_date FROM `job` j join division d on j.job_id=d.job_id where d.job_id=?");
