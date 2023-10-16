f1d6acc60c7cbd13f391544698f7a7f724e579ca
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-16 12:47:54 -0500

    Update model-employees-with-jobs.php

--- model-employees-with-jobs.php
+++ model-employees-with-jobs.php
@@ -16,7 +16,7 @@ function selectEmployees() {
 function selectJobsOfEmployees($iid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT j.job_id, job_number, job_title, start_date, office_number, building FROM `job` j join division d on j.job_id=d.job_id where d.employee_id=?");
+        $stmt = $conn->prepare("SELECT j.job_id, job_number, job_title, start_date, building FROM `job` j join division d on j.job_id=d.job_id where d.employee_id=?;");
         $stmt->bind_param("i", $iid);  
       $stmt->execute();
         $result = $stmt->get_result();
