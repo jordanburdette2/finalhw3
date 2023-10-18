33b30fa26cbd2aff2ada26fa5639fdcea8255821
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-18 14:06:36 -0500

    Update view-employees-with-jobs.php

--- view-employees-with-jobs.php
+++ view-employees-with-jobs.php
@@ -5,7 +5,6 @@
   while ($employee = $employees->fetch_assoc()) {
     ?>
     <div class="card">
-    <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title"><?php echo $employee['employee_name']; ?></h5>
       <p class="card-text">
