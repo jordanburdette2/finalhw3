42f4c1f557a0c80b3654b768e7a3ddf360fafc2f
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-18 13:56:19 -0500

    Update divisions-of-jobs.php

--- divisions-of-jobs.php
+++ divisions-of-jobs.php
@@ -5,6 +5,6 @@ require_once("model-divisions-of-jobs.php");
 $pageTitle = "Divisions of Jobs";
 include "view-header.php";
 $jobs = selectDivisionsOfJobs($_POST['jid']);
-include "view-divisons-of-jobs.php";
+include "view-divisions-of-jobs.php";
 include "view-footer.php";
 ?>
