fea8789f188b49b1a2d421d13c7f5bbc9c7f8e85
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-18 14:36:15 -0500

    Update model-companies.php

--- model-companies.php
+++ model-companies.php
@@ -2,7 +2,7 @@
 function selectCompanies() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT company_id, company_name, company FROM `company`");
+        $stmt = $conn->prepare("SELECT company_id, company_name, company_type FROM `company`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
