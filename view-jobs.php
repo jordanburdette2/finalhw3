2af83cb7c24f8269667a2abfb2832d9b3ffcf230
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-23 14:19:02 -0500

    Update view-jobs.php

--- view-jobs.php
+++ view-jobs.php
@@ -1,5 +1,5 @@
 <div class="row">
-  <div class="col>
+  <div class="col">
     <h1>Jobs</h1>
     </div>
     <div class="col=auto">
@@ -8,9 +8,7 @@
           <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
         </svg>
     </div>
-  
 </div>
-
 <div class="table-responsive">
   <table class="table">
     <thead>
