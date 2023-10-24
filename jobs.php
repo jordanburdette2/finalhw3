e7fa5d2d133a857428fe167faa15d264797cb3e0
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-24 11:09:17 -0500

    Update jobs.php

--- jobs.php
+++ jobs.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertCourse($_POST['jNumber'], $_POST['jTitle'], $_POST['jStart']);
+      insertJob($_POST['jNumber'], $_POST['jTitle'], $_POST['jStart']);
       break;
   }
 }
