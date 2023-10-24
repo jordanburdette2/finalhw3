621239d2542739eca397041a44316c9148d93d02
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-24 11:05:53 -0500

    Update jobs.php

--- jobs.php
+++ jobs.php
@@ -4,6 +4,15 @@ require_once("model-jobs.php");
 
 $pageTitle = "Jobs";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_PST['actionType']) {
+    case "Add":
+      insertCourse($_POST['jNumber'], $_POST['jTitle'], $_POST['jStart']);
+      break;
+  }
+}
+
 $jobs = selectJobs();
 include "view-jobs.php";
 include "view-footer.php";
