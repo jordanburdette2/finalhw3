1d18cbe7d0d7e053eb0f38d83b03f42732f0cd15
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-24 11:17:29 -0500

    Update jobs.php

--- jobs.php
+++ jobs.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertJob($_POST['jNumber'], $_POST['jTitle'], $_POST['jStart']);
+      if (insertJob($_POST['jNumber'], $_POST['jTitle'], $_POST['jStart'])) {
+        echo '<div class="alert alert-success" role="alert"> Job Added! </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error! </div>';
+      }
       break;
   }
 }
