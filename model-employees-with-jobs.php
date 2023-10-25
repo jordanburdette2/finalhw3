e954c7df92ae7e9c2fd668c5f5061084d7b74d94
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-25 09:51:37 -0500

    Update model-employees-with-jobs.php

--- model-employees-with-jobs.php
+++ model-employees-with-jobs.php
@@ -28,4 +28,47 @@ function selectJobsOfEmployees($iid) {
     }
 }
 
+function insertDivision($jid, $jobTitle, $startDate, $officeNum) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `job` ('job_id, `job_title`, `start_date`, 'office_number') VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("isss", $jid, $jobTitle, $startDate, $officeNum);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateDivision($iid, $jid, $jobTitle, $startDate, $officeNum) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `job` SET `job_number` = ?, `job_title` = ?, `start_date` = ?, 'office_number' = ? WHERE job_id=?");
+        $stmt->bind_param("iisss", $iid, $jid, $jobTitle, $startDate, $officeNum);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteDivision($did) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM division WHERE division_id=?");
+        $stmt->bind_param("i", $did);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 ?>
