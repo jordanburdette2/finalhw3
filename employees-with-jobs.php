6b62351fb21bca5b939c724658d48ab8095d7699
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-25 14:09:37 -0500

    Update employees-with-jobs.php

--- employees-with-jobs.php
+++ employees-with-jobs.php
@@ -8,3 +8,42 @@ $employees = selectEmployeesWithJobs();
 include "view-employees-with-jobs.php";
 include "view-footer.php";
 ?>
+
+
+<?php
+require_once("util-db.php");
+require_once("model-employees-with-jobs.php");
+
+$pageTitle = "Employees with Jobs";
+include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertDivision($_POST['jNumber'], $_POST['jTitle'], $_POST['jStart'])) {
+        echo '<div class="alert alert-success" role="alert"> Job Added! </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error! </div>';
+      }
+      break;
+    case "Edit":
+      if (updateDivision($_POST['jNumber'], $_POST['jTitle'], $_POST['jStart'], $_POST['jid'])) {
+        echo '<div class="alert alert-success" role="alert"> Job edited! </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error! </div>';
+      }
+      break;
+        case "Delete":
+      if (deleteDivision($_POST['jid'])) {
+        echo '<div class="alert alert-success" role="alert"> Job Deleted! </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error! </div>';
+      }
+      break;
+  }
+}
+
+$jobs = selectJobs();
+include "view-jobs.php";
+include "view-footer.php";
+?>
