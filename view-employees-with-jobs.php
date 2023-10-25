fdcf1663a78dd6ecc4e3ceb05bf7ece299e8b280
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-25 10:41:35 -0500

    Update view-employees-with-jobs.php

--- view-employees-with-jobs.php
+++ view-employees-with-jobs.php
@@ -1,22 +1,51 @@
+  <div class="row">
+  <div class="col">
 <h1>Employees with Jobs</h1>
-
+  </div>
+  <div class="col-auto">
+<?php
+include "view-employees-with-jobs-newform.php";
+?>
+  </div>
+</div>
 <div class="card-group">
-  <?php
-  while ($employee = $employees->fetch_assoc()) {
-    ?>
-    <div class="card">
+<?php
+while ($employee = $employees->fetch_assoc()) {
+?>
+  <div class="card">
     <div class="card-body">
       <h5 class="card-title"><?php echo $employee['employee_name']; ?></h5>
       <p class="card-text">
       <ul class="list-group">
-        
-  <?php
-    $jobs = selectJobsOfEmployees($employee['employee_id']);
-    
+<?php
+  $jobs = selectJobsOfEmployees($employee['employee_id']);
   while ($job = $jobs->fetch_assoc()) {
-    ?>
-      <li class="list-group-item"><?php echo $job['job_title']; ?> - <?php echo $job['start_date'];  ?></li>
-  <?php
+?>
+    <li class="list-group-item">
+    <div class="row">
+      <div class="col">
+        <?php echo $job['job_number']; ?> - <?php echo $job['job_title']; ?> - <?php echo $job['start_date']; ?>
+      </div>
+      <div class="col-auto">
+<?php
+include "view-employees-with-jobs-editform.php";
+?>
+      </div>
+      <div class="col-auto">
+        <form method="post" action="">
+          <input type="hidden" name="did" value="<?php echo $job['division_id']; ?>">
+          <input type="hidden" name="actionType" value="Delete">
+          <button type="submit" class="btn" onclick="return confirm('Are you sure?');">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+            </svg>
+          </button>
+        </form>
+      </div>
+    </div>
+    </li>
+<?php
   }
 ?>
       </ul>
