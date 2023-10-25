c616032bfb3b9f776bcc6e39bec12bf111532944
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-25 14:11:26 -0500

    Update employees-with-jobs.php

--- employees-with-jobs.php
+++ employees-with-jobs.php
@@ -2,18 +2,6 @@
 require_once("util-db.php");
 require_once("model-employees-with-jobs.php");
 
-$pageTitle="Employees with Jobs";
-include "view-header.php";
-$employees = selectEmployeesWithJobs();
-include "view-employees-with-jobs.php";
-include "view-footer.php";
-?>
-
-
-<?php
-require_once("util-db.php");
-require_once("model-employees-with-jobs.php");
-
 $pageTitle = "Employees with Jobs";
 include "view-header.php";
 
@@ -43,7 +31,7 @@ if (isset($_POST['actionType'])) {
   }
 }
 
-$jobs = selectJobs();
-include "view-jobs.php";
+$employees = selectEmployeesWithJobs();
+include "view-employees-with-jobs.php";
 include "view-footer.php";
 ?>
