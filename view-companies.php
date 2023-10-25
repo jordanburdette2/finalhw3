44571ac661a0e164ca989cc9ea7162b92037d4ae
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-10-25 14:34:02 -0500

    Update view-companies.php

--- view-companies.php
+++ view-companies.php
@@ -47,7 +47,9 @@ while ($company = $company->fetch_assoc()) {
       </button>
     </form>
     </td>
-      
+      <form method="post" action="employees-with-jobs.php">
+        <input type="hidden" name="cid" value="<?php echo $company['company_id']; ?>">
+      <button type="submit" class="btn btn-primary">Companies</button>
     </td>
   </tr>
 <?php
