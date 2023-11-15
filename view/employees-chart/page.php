b401ae5d51ff192f84ea48c1b88cc305325711f4
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-15 14:32:51 -0600

    Update page.php

--- view/employees-chart/page.php
+++ view/employees-chart/page.php
@@ -7,9 +7,6 @@
 
 <div><canvas class="zdog-canvas" width="240" height="240"></canvas></div>
 
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-     crossorigin=""/>
 </head>
 
 <body>
@@ -18,7 +15,7 @@
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script src="https://unpkg.com/zdog@1/dist/zdog.dist.min.js"></script>
 <script src="zdog-demo.js"></script>
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+
 
 <script>
   const ctx = document.getElementById('myChart');
@@ -139,6 +136,15 @@ new Zdog.Rect({
 
 </script>
 
+<script>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
+
+  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+       
+</script>
+  
   <div id="map">
     var map = L.map('map').setView([51.505, -0.09], 13);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
