070174e6040d210c9fd5994a20ed33f79517c7d6
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-15 09:54:38 -0600

    Create employee-chart-db.php

---
+++ model/employee-chart-db.php
@@ -0,0 +1,15 @@
+<?php
+function selectEmployees() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT employee_name, count(job_id) as num_divisions FROM employee e join division d on e.employee_id = d.employee_id GROUP BY employee_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
