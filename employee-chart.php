356499ceae898ed6aa7d967a633e3e2ee19e0f21
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-15 10:17:01 -0600

    Update employee-chart.php

--- employee-chart.php
+++ employee-chart.php
@@ -1,10 +1,10 @@
 <?php
 require_once("model/connection.php");
-require_once("model/employees-chart-db.php");
+require_once("model/employee-chart-db.php");
 
 $pageTitle = "Employees Chart"
 include "view/header.php"
 $employees = selectEmployees();
-include "view/employees-chart/page.php";
+include "view/employee-chart/page.php";
 include "view/footer.php";
 ?>
