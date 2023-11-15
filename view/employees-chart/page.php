9b8cbc4f7bfc9833bb9657e5892d560f29bbce07
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-15 14:24:57 -0600

    Update page.php

--- view/employees-chart/page.php
+++ view/employees-chart/page.php
@@ -134,8 +134,6 @@ new Zdog.Rect({
 });
 
   <div id="map">
-  var map = L.map('map').setView([51.505, -0.09], 13);
-
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
