a628f1f2e52176b151c3b6bec10e74aef302658a
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-15 09:30:07 -0600

    Update jobs-of-employees.php

--- jobs-of-employees.php
+++ jobs-of-employees.php
@@ -3,8 +3,8 @@ require_once("util-db.php");
 require_once("model/jobs-of-employees.php");
 
 $pageTitle = "Jobs of Employees";
-include "header.php";
+include "view/header.php";
 $jobs = selectJobsOfEmployees($_GET['id']);
-include "jobs-of-employees.php";
-include "footer.php";
+include "view/jobs-of-employees.php";
+include "view/footer.php";
 ?>
