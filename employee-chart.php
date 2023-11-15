d3a5fd1251d993715d2490addc97b11a62479b55
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-15 09:45:06 -0600

    Create employee-chart.php

---
+++ employee-chart.php
@@ -0,0 +1,10 @@
+<?php
+require_once("model/connection.php");
+require_once("model/employees-chart-db.php");
+
+$pageTitle = "Employees Chart"
+include "view/header.php"
+$employees = selectEmployees();
+include "view/employees-chart/page.php";
+include "view/footer.php";
+?>
