cfb73f4c64e604de40b98bdae7d4dd2cf7a22d63
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-15 14:12:26 -0600

    Update page.php

--- view/employees-chart/page.php
+++ view/employees-chart/page.php
@@ -81,7 +81,22 @@ swal({
   }
 });
 
+let illo = new Zdog.Illustration({
+  // set canvas with selector
+  element: '.zdog-canvas',
+});
 
+// add circle
+new Zdog.Ellipse({
+  addTo: illo,
+  diameter: 80,
+  stroke: 20,
+  color: '#636',
+});
+
+// update & render
+illo.updateRenderGraph();
+  
 new Zdog.Ellipse({
   addTo: illo,
   diameter: 80,
