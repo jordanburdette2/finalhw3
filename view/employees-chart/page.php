23dd349d3507ce1c26264c93cc2c20bd062d4cbb
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-15 10:13:51 -0600

    Update page.php

--- view/employees-chart/page.php
+++ view/employees-chart/page.php
@@ -1,10 +1,37 @@
-   <h1>Employees chart</h1>
+<h1>Employees chart</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-  <?php
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+    datasets: [{
+        data: [
+<?php
 while ($employee = $employees->fetch_assoc()) {
-  
-
+  echo $employee['num_divisions'] . ", ";
+}
 ?>
+        ]
+    }],
 
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+<?php
+$employee = selectEmployees();
+while ($employee = $employees->fetch_assoc()) {
+echo "'" . $employee['employee_name'] . "', ";
+}
+?>
+    ]
+},
+  });
+</script>
     
     
