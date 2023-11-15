eeed37068a81d7aaa5f0125887d4e39194d29b55
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-15 14:22:06 -0600

    Update page.php

--- view/employees-chart/page.php
+++ view/employees-chart/page.php
@@ -6,11 +6,16 @@
 
 <div><canvas class="zdog-canvas" width="240" height="240"></canvas></div>
 
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
+
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script src="https://unpkg.com/zdog@1/dist/zdog.dist.min.js"></script>
 <script src="zdog-demo.js"></script>
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 
 <script>
   const ctx = document.getElementById('myChart');
@@ -127,6 +132,15 @@ new Zdog.Rect({
   color: '#E62',
   fill: true,
 });
+
+  <div id="map"></div>
+  #map { height: 180px; }
+  var map = L.map('map').setView([51.505, -0.09], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 </script>
     
