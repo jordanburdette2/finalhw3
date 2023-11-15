86eae1690dbd20256b98649ae087a085a594b2e6
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-15 10:43:36 -0600

    Update employee-chart-db.php

--- model/employee-chart-db.php
+++ model/employee-chart-db.php
@@ -2,7 +2,7 @@
 function selectEmployees() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT employee_name, count(job_id) as num_divisions FROM employee e join division d on e.employee_id = d.employee_id GROUP BY employee_name");
+        $stmt = $conn->prepare("SELECT employee_name, count(division_id) as num_divisions FROM employee e join division d on e.employee_id = d.employee_id GROUP BY employee_name");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
